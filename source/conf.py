4246b017d7cd8bf7b228924560a0b6fdc4cb264b
Uwe Fladrich <uwe.fladrich@smhi.se>
2021-05-10 15:12:46 +0200

    Use Sphinx autosectionlabel

--- source/conf.py
+++ source/conf.py
@@ -29,6 +29,7 @@ author = 'Uwe Fladrich (SMHI)'
 # ones.
 extensions = [
     'sphinx_rtd_theme',
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
